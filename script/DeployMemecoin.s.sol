131363e2c337c0dc2f4af88aacf341ff201f052e
Arkano.eth <62271657+arkanoeth@users.noreply.github.com>
2024-08-06 16:07:20 +0200

    Adding Staking deplyment script

--- script/DeployMemecoin.s.sol
+++ script/DeployMemecoin.s.sol
@@ -3,16 +3,22 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 import "../src/Memecoin.sol";
+import "../src/Staking.sol";
 
-contract DeployMemecoin is Script {
+contract DeployMemecoinAndStaking is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        string memory privateKeyStr = vm.envString("PRIVATE_KEY");
+        uint256 deployerPrivateKey = vm.parseUint(privateKeyStr);
         vm.startBroadcast(deployerPrivateKey);
 
+        // Deploy Memecoin
         string memory metadataURI = "ipfs://Qm...";  // Replace with the actual IPFS hash or URI
         Memecoin memecoin = new Memecoin(msg.sender, metadataURI);
-
         console.log("Memecoin deployed to:", address(memecoin));
+
+        // Deploy Staking contract
+        Staking staking = new Staking(msg.sender, address(memecoin), address(memecoin));
+        console.log("Staking contract deployed to:", address(staking));
 
         vm.stopBroadcast();
     }
